ad6bff001a7371f8f18f1cbcb2fb1b5b0d1d07a6
sihle <gubug777@gmail.com>
2022-04-16 11:49:47 +0800

    update PostModel, hide version key

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,43 +1,48 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
   // Data Schema
-  tags: [
-    {
+  {
+    tags: [
+      {
+        type: String,
+        required: [true, '貼文標籤為必填']
+      }
+    ],
+    type: {
       type: String,
-      required: [true, '貼文標籤為必填']
+      enum: ['group', 'person']
+    },
+    name: {
+      type: String,
+      required: [true, "貼文名稱為必填"]
+    },
+    content: {
+      type: String,
+      required: [true, '貼文內容為必填']
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
     }
-  ],
-  type: {
-    type: String,
-    enum: ['group', 'person']
   },
-  name: {
-    type: String,
-    required: [true, "貼文名稱為必填"]
-  },
-  content: {
-    type: String,
-    required: [true, '貼文內容為必填']
-  },
-  image: {
-    type: String,
-    default: ''
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    select: false
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments: {
-    type: Number,
-    default: 0
+  {
+    versionKey: false
   }
-});
+);
 
 const PostModel = mongoose.model('Post', postSchema);
 
