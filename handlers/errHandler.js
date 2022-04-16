fa98a64533eee130905add22268f6d387b0a5eec
sihle <gubug777@gmail.com>
2022-04-16 12:23:10 +0800

    update errHandler

--- handlers/errHandler.js
+++ handlers/errHandler.js
@@ -1,17 +1,17 @@
 const headers = require('../headers/corsHeader');
 
 const errHandler = (res, statusCode, errors={}) => {
+  const errContentInfo = {
+    400: "請檢查資料格式，格式有誤！",
+    404: "該頁面不存在"
+  }
   const data = {
     status: 'false',
     errCode: statusCode,
     message: ''
   }
-  
-  if (statusCode && Object.keys(errors).length === 0) {
-    data.message = "請檢查資料格式，格式有誤！";
-  } else {
-    data.message = errors.message;
-  }
+
+  data.message = errors.message || errContentInfo[statusCode];
 
   res.writeHead(statusCode, headers);
   res.write(JSON.stringify(data));
